a5cdb96c92ba8b2291e8b40b91e408aacf55477f
Matthew Cole <mcole8@binghamton.edu>
2022-11-21 00:37:33 -0600

    Update test.c with stack, queue tests

--- test.c
+++ test.c
@@ -81,20 +81,20 @@ void testVECTOR(void) {
   // Test removal
   remove_Vector(myVector, 3);
   CU_ASSERT_EQUAL(myVector->used[3], false);
-  CU_ASSERT_EQUAL(sz,myVector->size);
-  CU_ASSERT_EQUAL(len,myVector->len);
+  CU_ASSERT_EQUAL(sz, myVector->size);
+  CU_ASSERT_EQUAL(len, myVector->len);
 
   // Test clearing
   clear_Vector(myVector);
   CU_ASSERT_EQUAL(sz, myVector->size);
-  CU_ASSERT_EQUAL(myVector->len,0);
+  CU_ASSERT_EQUAL(myVector->len, 0);
 
   // Test deletion
   del_Vector(myVector);
   CU_ASSERT_PTR_NULL(myVector);
 }
 
-void testLIST(void) { 
+void testLIST(void) {
   Node *myNode = NULL;
 
   // Test node creation and deletion
@@ -111,22 +111,21 @@ void testLIST(void) {
   CU_ASSERT_PTR_NOT_NULL(myList);
 
   // Test list appending
-  for (size_t i = 0; i < 10; ++i)
-  {
+  for (size_t i = 0; i < 10; ++i) {
     append_Node(myList, i);
   }
 
   // Check forward traversal
   Node *nptr = myList->head;
-  for (size_t i = 0; i <= 10; ++i){
-    CU_ASSERT_EQUAL(nptr->data,(Data) i);
+  for (size_t i = 0; i <= 10; ++i) {
+    CU_ASSERT_EQUAL(nptr->data, (Data)i);
     nptr = nptr->next;
   }
 
   // Check reverse traversal
   nptr = myList->tail;
-  for (size_t i = 10; i >= 0; --i){
-    CU_ASSERT_EQUAL(nptr->data,(Data) i);
+  for (size_t i = 10; i >= 0; --i) {
+    CU_ASSERT_EQUAL(nptr->data, (Data)i);
     nptr = nptr->prev;
   }
 
@@ -157,9 +156,68 @@ void testLIST(void) {
   CU_ASSERT_PTR_NULL(myList);
 }
 
-void testSTACK(void) { CU_PASS("Hello world from Stacks!"); }
+void testSTACK(void) {
+  // Initialization
+  myStack = new_Stack();
+  CU_ASSERT_PTR_NOT_NULL_FATAL(myStack);
+  CU_ASSERT_EQUAL(myStack->length, 0);
+  CU_ASSERT_PTR_NULL(myStack->top);
 
-void testQUEUE(void) { CU_PASS("Hello world from Queues!") }
+  // Adding Elements to the Stack
+  for (size_t i = 0; i < 5; ++i) {
+    push_Stack(myStack, (Data)i);
+  }
+  CU_ASSERT_EQUAL(myStack->length, 5);
+  CU_ASSERT_EQUAL(peek_Stack(myStack), (Data)4);
+
+  // Removing Elements from the Stack
+  for (size_t i = 4; i > 2; --i) {
+    CU_ASSERT_EQUAL(pop_Stack(myStack), (Data)i);
+  }
+  CU_ASSERT_EQUAL(myStack->length, 3);
+
+  // Clearing the Stack
+  clear_Stack(myStack);
+  CU_ASSERT_PTR_NOT_NULL(myStack);
+  CU_ASSERT_EQUAL(myStack->length, 0);
+  CU_ASSERT_PTR_NULL(myStack->top);
+
+  // Finalization
+  del_Stack(myStack);
+  CU_ASSERT_PTR_NULL(myStack);
+}
+
+void testQUEUE(void) {
+  // Initialization
+  myQueue = new_Queue();
+  CU_ASSERT_PTR_NOT_NULL_FATAL(myQueue);
+  CU_ASSERT_EQUAL(myQueue->length, 0);
+  CU_ASSERT_PTR_NULL(myQueue->head);
+  CU_ASSERT_PTR_NULL(myQueue->tail);
+
+  // Adding Elements to the Queue
+  for (size_t i = 0; i < 5; ++i) {
+    enqueue_Queue(myQueue, (Data)i);
+  }
+  CU_ASSERT_EQUAL(myQueue->length, 5);
+  CU_ASSERT_EQUAL(peek_Queue(myQueue), (Data)4);
+
+  // Removing Elements from the Queue
+  for (size_t i = 0; i < 2; ++i) {
+    CU_ASSERT_EQUAL(dequeue_Queue(myQueue), (Data)i);
+  }
+  CU_ASSERT_EQUAL(myQueue->length, 3);
+
+  // Clearing the Queue
+  clear_Queue(myQueue);
+  CU_ASSERT_PTR_NOT_NULL(myQueue);
+  CU_ASSERT_EQUAL(myQueue->length, 0);
+  CU_ASSERT_PTR_NULL(myQueue->top);
+
+  // Finalization
+  del_Queue(myQueue);
+  CU_ASSERT_PTR_NULL(myQueue);
+}
 
 int main(int argc, char *argv[]) {
 
